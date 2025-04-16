8eb2e388d25d1c10c58363251f2b8f982b98bdae
YanGraim <yangraim@gmail.com>
2025-04-16 10:02:38 -0300

    utiliza jest para testes

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,30 @@
 
-// cruar um bloco que agrupa varios testes relacionados
+function sum(n1: number, n2: number) {
+    return n1 + n2
+}
+
+
+function media(n1: number, n2: number) {
+    const resultado = (n1 + n2) / 2
+    if (resultado >= 7) {
+        return "aprovado"
+    } else {
+        return "exame"
+    }
+}
+
+
+// criar um bloco que agrupa varios testes relacionados
 describe("First teste app component", () => {
-    it("should add 1 + 2 to equal 3", () => {
-        expect(1).toBe(1)
+    it("should add 5 + 2 to equal 7", () => {
+        expect(sum(5, 2)).toBe(7)
     })
+
+    it("should calculate the average and return exame", () => {
+        expect(media(5, 6)).toBe("exame")
+        expect(media(7, 8)).toBe("aprovado")
+    })
+
 });
 
 export default {}
--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <div>
       <h1>Configurando testes</h1>
+      <p>teste</p>
     </div>
   )
 }
