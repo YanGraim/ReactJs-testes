cb7f1bf663351ea99630e32203248fe99830d887
YanGraim <yangraim@gmail.com>
2025-04-16 09:24:21 -0300

    cria teste

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+
+// cruar um bloco que agrupa varios testes relacionados
+describe("First teste app component", () => {
+    it("should add 1 + 2 to equal 3", () => {
+        expect(1).toBe(1)
+    })
+});
+
+export default {}
