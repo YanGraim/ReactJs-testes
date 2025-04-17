f476e4d8a16cf7249282dc4640d1421175e71000
YanGraim <yangraim@gmail.com>
2025-04-17 09:14:47 -0300

    adiciona mais testes ao componente User

--- src/User/User.test.tsx
+++ src/User/User.test.tsx
@@ -4,6 +4,24 @@ import { User } from "."
 
 
 describe("User component", () => {
+
+    it("Should check if the name is not displayed when component is mounted", () => {
+        render(<User />)
+
+        const message = screen.queryByText("Usuário online: Yan Graim")
+        expect(message).not.toBeInTheDocument();
+    })
+
+    it("Should test if the value typed in the input is correct", () => {
+        render(<User />)
+
+        const inputElement = screen.getByPlaceholderText("Digite o nome")
+
+        fireEvent.change(inputElement, { target: { value: "Yan Graim" } })
+
+        expect(inputElement).toHaveValue("Yan Graim")
+    })
+
     it("should display the username after typing in the input and click on the button", () => {
         render(<User />)
 
