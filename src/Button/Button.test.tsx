891804cb65ec50f15432a24a333466a0149b6f90
YanGraim <yangraim@gmail.com>
2025-04-17 08:25:07 -0300

    testa estilos do componente

--- src/Button/Button.test.tsx
+++ src/Button/Button.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom"
 import { Button } from ".";
 
 describe("Button component", () => {
@@ -12,6 +13,14 @@ describe("Button component", () => {
         fireEvent.click(button);
 
         expect(onclick).toHaveBeenCalledTimes(2);
+    })
+
+    it("Should render with gray background if disabled", () => {
+        render(<Button onClick={() => { }} disabled={true}>Meu botao</Button>)
+
+        const button = screen.getByRole("button", { name: "Meu botao" })
+
+        expect(button).toHaveStyle({ backgroundColor: "#fafafa" })
     })
 })
 
